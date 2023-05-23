a09662ae1092971915928d5038ba97caf43c0e1f
SWalaszek <105596557+SWalaszek@users.noreply.github.com>
2023-05-23 18:44:27 +0200

    Create variables.tf

---
+++ azure-blob-storage/variables.tf
@@ -0,0 +1,6 @@
+variable "resource_group_name" {}
+variable "location" {}
+variable "storage_account_name" {}
+variable "storage_container_name" {}
+variable "storage_blob_name" {}
+variable "storage_blob_type" {}
